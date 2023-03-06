5ec5d3d1cd3a3495b5773ece26bd7b8639759fa7
moravechynek <moravechynek@seznam.cz>
2023-03-06 10:44:59 +0100

    Stopwords removal

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 # Vocabulary
-
-## Usage
-
-| Command                        | Description |
-| ------------------------------ | ----------- |
-| `vocabulary -i vocab.txt data` | input       |
-| `vocabulary -e vocab.txt data` | prints the percentage of known words        |
--- pos_tag.py
+++ pos_tag.py
@@ -1,18 +1,29 @@
 import nltk
 from nltk import word_tokenize
 
-nltk.download('punkt')
+"""nltk.download('punkt')
 nltk.download('average_percentron_tagger')
-nltk.download('universal_tagset')
+nltk.download('universal_tagset')"""
 
-text = "This is one simple example."
+def rm_stopwords(list):
+    stopwords = ['?',';','\n','\n\n','!','.',',',':']
+    for item in list:
+        if item[0] in stopwords:
+            list.remove(item)
+    return list
+
+text = "This is one simple example. Yes, very simple example."
 tokens = word_tokenize(text)
-tokens = [t.lower() for t in tokens]
-tokens.sort()
 
 tags = nltk.pos_tag(tokens, tagset = 'universal')
 
+tags = [ [t[0].lower(),t[1]] for t in tags]
+
+tags = rm_stopwords(tags)
+
+tags.sort()
+
 f = open('myVocabulary.txt', 'w')
 for pair in tags:
-    f.write(f'{pair[0]} - {pair[1]}')
+    f.write(f'{pair[0]} - {pair[1]}\n')
 f.close()
